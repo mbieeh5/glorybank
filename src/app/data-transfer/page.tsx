bdcd14a95a547e09fd067e084164ebdf48817c37
mbieeh5 <mbieeh5@gmail.com>
2025-02-05 21:49:10 +0700

    update-V4.0.0-Lite

--- src/app/data-transfer/page.tsx
+++ src/app/data-transfer/page.tsx
@@ -1,11 +1,20 @@
+import MutasiHarian from "@/components/MutasiHarian";
+import ReminingBalance from "@/components/ReminingBalance";
+import TableBank from "@/components/TableBank";
 import React from "react";
 
 
 
 export default function DataTransfer() {
     return(
-        <>
-        DATA TRANSFER
-        </>
+        <div className="pt-2">
+            <div className="flex justify-between m-2 p-2">
+                <ReminingBalance />
+                <MutasiHarian />
+            </div>
+            <div className="pt-2">
+                <TableBank />
+            </div>
+        </div>
     )
 }
