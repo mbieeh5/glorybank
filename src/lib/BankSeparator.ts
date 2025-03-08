ef8952d20b5ed308754d46ee4b7423c169d4243e
mbieeh5 <mbieeh5@gmail.com>
2025-03-08 11:13:56 +0700

    addeed mandiri function

--- src/lib/BankSeparator.ts
+++ src/lib/BankSeparator.ts
@@ -2,8 +2,8 @@ export const BankSeparator = (bank:string) => {
     const bankBCA = bank.match(/BCA(?!.*DIGITAL)(?!.*SYARIAH)/i) ? "BCA" : null;
     const bankBRI = bank.match(/BRI/i) ? "BRI" : null;
     const bankBNI = bank.match(/BNI/i) ? "BNI" : null;
-    //const bankMandiri = bank.match(/MANDIRI/i) ? "MANDIRI" : null;
+    const bankMandiri = bank.match(/MANDIRI/i) ? "MANDIRI" : null;
     const bankDanamon = bank ? "DANAMON" : null;
-    const sanitizerBank = bankBCA || bankBRI || bankBNI || bankDanamon;
+    const sanitizerBank = bankBCA || bankBRI || bankBNI || bankMandiri || bankDanamon;
     return sanitizerBank
 }
